f2376660b27bdc987c862c9d48920f0b12ed0009
Órla Nugent <orla@bulb.co.uk>
2021-09-23 18:13:29 +0100

    Creating Post and User classes

---
+++ posts.js
@@ -0,0 +1,12 @@
+/// Posts include a title, a date, an author and text 
+
+class Posts {
+    constructor (title, date, author, text) {
+        this.title = title,
+        this.date = date,
+        this.author = username, 
+        this.text = text; 
+    }
+}
+
+console.log(Posts);
---
+++ users.js
@@ -0,0 +1,8 @@
+/// Users - have usernames and can write posts 
+
+class Users {
+    constructor (username) {
+        this.username = username; 
+    }
+}
+
